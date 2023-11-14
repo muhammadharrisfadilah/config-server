802015806e2f6d41a599dd98aa0e2ba267f43a98
harris <harris030999@example.com>
2023-11-14 11:40:51 +0700

    starter project

---
+++ src/test/java/com/yaossstudio/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaossstudio.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
